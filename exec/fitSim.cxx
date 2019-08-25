f97701d21e05680982c49deca613641d95c239c7
Guillaume Garillot <gari.guillaume@gmail.com>
2019-08-25 19:07:51 +0200

    cleaned fitSim

--- exec/fitSim.cxx
+++ exec/fitSim.cxx
@@ -20,28 +20,19 @@ int main(int argc , char** argv)
 	if ( argc != 2 )
 	{
 		std::cerr << "ERROR : problem with arguments passed for the program" << std::endl ;
-		return -1 ;
+		return 1 ;
 	}
 
 	std::string inputFile = argv[1] ;
 	std::string outputFile = std::string("Fit.root") ;
 
-//	double qbar = atof(argv[1]) ;
-//	double delta = atof(argv[2]) ;
-//	double d = atof(argv[3]) ;
-
 	GraphManager a ;
 
-//	std::stringstream filePath ; filePath << "/home/garillot/files/PolyaScan/MulResults/map_" << qbar << "_" << delta << "_" << d << ".root" ;
-//	a.ProcessFile( filePath.str() ) ;
     a.ProcessSimu( inputFile ) ;
 
 	a.fitAllEffGraphs() ;
 	a.fitAllMulGraphs() ;
 
-	std::cout << "write tree" << std::endl ;
-//	std::stringstream outFilePath ; outFilePath << "/home/garillot/files/PolyaScan/Fits/Fits_" << qbar << "_" << delta << "_" << d << ".root" ;
-//	a.writeResultTree( outFilePath.str() ) ;
 	a.writeResultTree( outputFile ) ;
 
 	return 0 ;
